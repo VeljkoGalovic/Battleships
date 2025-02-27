44b7e570c57bd9ec72130f5e0a18e20147c29e26
VeljkoGalovic <galovicveljko@gmail.com>
2025-02-27 15:40:55 +0100

    Apdejt fajla Glavni_program.cpp
    Dodao sam rekurziju, tehnicki je projekat zavrsen, ali se treba dodati bolje odsecanje, pretvoriti sve u jedan veliki .exe fajl i mozda prepisati pajton u cpp ako Dusko bude izmisljao.

--- Glavni_program.cpp
+++ Glavni_program.cpp
@@ -1,10 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
-char MatricaKaraktera[100][100];
+char MatricaKaraktera[100][100], KopijaMatrice[100][100], Resenje[100][100];
 int X, Y, Red[100], Kolona[100], br, BrBrodova;
 bool Provera1 = 1, Provera2 = 1, Provera3 = 1;
 string str;
-
+stack<int> stek;
+int Velicina, Jedinice, Dvojke, Trojke, Cetvorke;
+bool pos[100][100];
 /// LEGENDA:
 /// e - POLUKRUG OKRENUT KA DESNO
 /// f - POLUKRUG OKRENUT KA DOLE
@@ -19,7 +21,7 @@ string str;
 void ULAZ()
 {
    memset(MatricaKaraktera, '#', sizeof(MatricaKaraktera));
-
+   memset(KopijaMatrice, '#', sizeof(KopijaMatrice));
    freopen("Ships.in", "r", stdin); /// UCITAVANJE IZ FAJLA
    cin >> X >> Y;
    for(int i=1; i<=Y; i++)
@@ -370,11 +372,214 @@ bool PROVERA_KVADRATA()
 
    return temp;
 }
+void KOPIRAJ_MATRICU()
+{
+   for(int i=1; i<=Y; i++)
+   {
+      for(int j=1; j<=X; j++)
+         KopijaMatrice[i][j] = MatricaKaraktera[i][j];
+   }
+}
+
+bool PUNA() /// AKO JE KOPIJA MATRICE ISPUNJENA U POTPUNOSTI
+{
+   for(int i=1; i<=Y; i++)
+   {
+      for(int j=1; j<=X; j++)
+      {
+         if(KopijaMatrice[i][j] == '.')
+            return 0;
+      }
+   }
+
+   return 1;
+}
+
+void DFS(int i, int j)
+{
+   pos[i][j] = 1;
+   stek.push(1);
+   //cout << i << " " << j << "->";
+   if(KopijaMatrice[i+1][j] != '#' and pos[i+1][j] == 0)
+      DFS(i+1, j);
+   if(KopijaMatrice[i-1][j] != '#' and pos[i-1][j] == 0)
+      DFS(i-1, j);
+   if(KopijaMatrice[i][j+1] != '#' and pos[i][j+1] == 0)
+      DFS(i, j+1);
+   if(KopijaMatrice[i][j-1] != '#' and pos[i][j-1] == 0)
+      DFS(i, j-1);
+}
+
+bool USLOVI()
+{
+   Jedinice = 4;
+   Dvojke = 3;
+   Trojke = 2;
+   Cetvorke = 1;
+   memset(pos, 0, sizeof(pos));
+
+   for(int i=1; i<=Y; i++)
+   {
+      for(int j=1; j<=X; j++)
+      {
+         if(KopijaMatrice[i][j] != '#' and pos[i][j] == 0) /// AKO JE BROD
+         {
+            while(stek.empty() == false)
+               stek.pop();
+            DFS(i, j);
+            Velicina = stek.size();
+            //cout << Velicina << "\n";
+            if(Velicina == 1)
+               Jedinice--;
+
+            if(Velicina == 2)
+               Dvojke--;
+
+            if(Velicina == 3)
+               Trojke--;
+
+            if(Velicina == 4)
+               Cetvorke--;
+         }
+      }
+   }
+
+
+   if(Jedinice == 0 and Dvojke == 0 and Trojke == 0 and Cetvorke == 0)
+      return 1;
+   return 0;
+}
+
+void KOPIRAJ_RESENJE()
+{
+   for(int i=1; i<=Y; i++)
+   {
+      for(int j=1; j<=X; j++)
+      {
+         if(KopijaMatrice[i][j] == '#')
+            Resenje[i][j] = '.';
+         else
+            Resenje[i][j] = 'b';
+      }
+   }
+}
+void ISPIS_KOPIJE()
+{
+   for(int i=1; i<=Y; i++)
+   {
+      for(int j=1; j<=X; j++)
+         cout << KopijaMatrice[i][j] << " ";
+      cout << "\n";
+   }
+   cout << "\n\n";
+}
+
+bool PROVERA_ZBIRA()
+{
+   int Brojac = 0;
+   for(int i=1; i<=Y; i++)
+   {
+      Brojac = 0;
+      for(int j=1; j<=X; j++)
+      {
+         if(KopijaMatrice[i][j] != '#')
+            Brojac++;
+      }
+
+      if(Red[i] != Brojac)
+         return 0;
+   }
+
+   for(int i=1; i<=Y; i++)
+   {
+      Brojac = 0;
+      for(int j=1; j<=X; j++)
+      {
+         if(KopijaMatrice[j][i] != '#')
+            Brojac++;
+      }
+
+      if(Kolona[i] != Brojac)
+         return 0;
+   }
+
+   return 1;
+}
+
+void REKURZIJA(int a, int b)
+{
+   /// PROBAJ SA BRODOM, DODAJ ODSECANJE DA NE MOZE AKO USLOVI NISU ISPUNJENI, TJ. AKO JE OVO > OD USLOVA
+   KopijaMatrice[a][b] = 'p';
+
+   //ISPIS_KOPIJE();
+   if(PUNA())
+   {
+      //cout << "a\n";
+      if(USLOVI() and PROVERA_ZBIRA())
+      {
+
+         KOPIRAJ_RESENJE();
+         return;
+      }
+      //cout << "b\n";
+   }
+
+   for(int i=1; i<=Y; i++)
+   {
+      for(int j=1; j<=X; j++)
+      {
+         if(KopijaMatrice[i][j] == '.')
+         {
+            REKURZIJA(i, j);
+            KopijaMatrice[i][j] = '.'; // MOZDA NE TREBA
+         }
+      }
+   }
+
+
+
+
+
+   /// PROBAJ SA VODOM
+   KopijaMatrice[a][b] = '#';
+   //ISPIS_KOPIJE();
+
+   if(PUNA())
+   {
+      if(USLOVI() and PROVERA_ZBIRA())
+      {
+         KOPIRAJ_RESENJE();
+         return;
+      }
+   }
+
+
+   for(int i=1; i<=Y; i++)
+   {
+      for(int j=1; j<=X; j++)
+      {
+         if(KopijaMatrice[i][j] == '.')
+         {
+            REKURZIJA(i, j); /// POZOVE REKURZIJU U OVOM MOMENTU AKO
+            KopijaMatrice[i][j] = '.'; // MOZDA NE TREBA
+         }
+      }
+   }
+}
+
+void ISPIS_RESENJA()
+{
+   for(int i=1; i<=Y; i++)
+   {
+      for(int j=1; j<=X; j++)
+         cout << Resenje[i][j] << " ";
+      cout << "\n";
+   }
+}
 
 int main()
 {
    ULAZ();
-   ISPIS_MATRICE();
    INICIJALIZACIJA();
 
    while(Provera1 or Provera2 or Provera3) /// SKENIRANJE - UPISUJE STVARI KOJE ZNA ZASIGURNO
@@ -384,7 +589,23 @@ int main()
       Provera3 = PROVERA_BRODOVA();
    }
 
+   KOPIRAJ_MATRICU(); /// RADI IZBEGAVANJA GRESAKA, MOZE SE UKLONITI KASNIJE
    /// OVDE JOS SAMO DODATI REKURZIJU
+   for(int i=1; i<=Y; i++)
+   {
+      for(int j=1; j<=X; j++)
+      {
+         if(KopijaMatrice[i][j] == '.')
+         {
+            REKURZIJA(i, j); /// DODAJ ODSECANJE
+            break;
+         }
+      }
+   }
+
    ISPIS_MATRICE();
+
+   cout << "\n\n\n. - voda, b - brod\n";
+   ISPIS_RESENJA();
    return 0;
 }
