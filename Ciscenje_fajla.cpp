b80aaa566994daa208023a9bc3f14a15c68c06a7
VeljkoGalovic <galovicveljko@gmail.com>
2025-04-07 02:07:02 +0200

    Delete Ciscenje_fajla.cpp

--- Ciscenje_fajla.cpp
+++
@@ -1,9 +0,0 @@
-#include<fstream>
-using namespace std;
-fstream ofs;
-int main()
-{
-   ofs.open("Source.txt", ios::out | ios::trunc);
-   ofs.close(); //Using microsoft incremental linker version 14
-   return 0;
-}
